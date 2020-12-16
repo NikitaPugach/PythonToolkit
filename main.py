66c8e848df78aac5a9e4461b76277931ff949ec7
Mykyta Pugach <mykyta.pugach@nixsolutions.com>
2020-12-16 16:54:41 +0200

    Make remove unproductive non terminal with list of lists

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ class Grammar(object):
         self.E = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '(', ')']
         self.N = ["FORMULA", "SIGN", "NUMBER", "DIGIT"]
         self.S = self.N[0]
-        self.rules = {self.S: [self.S, self.N[1], self.S], }
+        self.rules = {self.N[2]: [self.N[1]], self.N[1]: self.E[0], self.S: [[self.S, self.N[1], self.S]]}
 
 
 class ToolKit(object):
@@ -16,11 +16,27 @@ class ToolKit(object):
         for rules_for_symbols in self.grammar.rules:
             right_part = self.grammar.rules[rules_for_symbols]
             is_terminal = True
-            for rule in right_part:
-                for symbol in rule:
-                    is_terminal = symbol in self.grammar.E    # Проверка все ли символы в правой части терминалы
-                    if not is_terminal:
-                        break
+            # for rule in right_part:
+            if isinstance(right_part, list):
+                for symbol in right_part:
+                    if isinstance(symbol, list):
+                        for s in symbol:
+                            is_terminal = s in self.grammar.E  # Проверка все ли символы в правой части терминалы
+                            if not is_terminal:
+                                break
+                        if is_terminal:  # если да, то добавляем левую часть в alive
+                            alive.append(rules_for_symbols)
+                            break
+                    else:
+                        is_terminal = symbol in self.grammar.E    # Проверка все ли символы в правой части терминалы
+                        if not is_terminal:
+                            break
+                if is_terminal:  # если да, то добавляем левую часть в alive
+                    alive.append(rules_for_symbols)
+                    break
+            else:
+                is_terminal = right_part in self.grammar.E  # Проверка все ли символы в правой части терминалы
+
                 if is_terminal:  # если да, то добавляем левую часть в alive
                     alive.append(rules_for_symbols)
                     break
@@ -32,15 +48,40 @@ class ToolKit(object):
             alive_size = len(alive)
             for rules_for_symbols in self.grammar.rules:
                 right_part = self.grammar.rules[rules_for_symbols]
-                for rule in right_part:
-                    is_terminal_or_from_alive = True
-                    for symbol in rule:
-                        is_terminal_or_from_alive = symbol in self.grammar.E or symbol in alive  # Проверка все ли
-                        if not is_terminal_or_from_alive:             # символы в правой части терминалы либо из alive
-                            break
+                # for rule in right_part:
+                is_terminal_or_from_alive = True
+                if isinstance(right_part, list):
+                    for symbol in right_part:
+
+                        if isinstance(symbol, list):
+                            for s in symbol:
+                                is_terminal_or_from_alive = s in self.grammar.E or s in alive  # Проверка все лисимволы
+                                if not is_terminal_or_from_alive:  # в правой части терминалы либо из alive
+                                    break
+                            if is_terminal_or_from_alive:  # если да, то добавляем левую часть в alive
+                                if not rules_for_symbols in alive:
+                                    alive.append(rules_for_symbols)
+                        else:
+                            is_terminal_or_from_alive = symbol in self.grammar.E or symbol in alive  # Проверка все ли
+                            if not is_terminal_or_from_alive:          # символы в правой части терминалы либо из alive
+                                break
+
                     if is_terminal_or_from_alive:  # если да, то добавляем левую часть в alive
-                        alive.append(rules_for_symbols)
+                        if not rules_for_symbols in alive:
+                            alive.append(rules_for_symbols)
+
+                else:
+                    is_terminal_or_from_alive = right_part in self.grammar.E or right_part in alive
+                    if is_terminal_or_from_alive:
+                        if not rules_for_symbols in alive:
+                            alive.append(rules_for_symbols)
+
             if alive_size == len(alive):   # Проверка расширилось ли alive
                 expansion = False
 
         return alive
+
+
+toolkit = ToolKit()
+
+print(toolkit.remove_unproductive_non_terminal())
